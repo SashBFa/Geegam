3d9e3f54af33d4b9376010d0a09d5fcc40b6e78b
SashBFa <bastienfajs@gmail.com>
2022-05-18 19:26:09 +0200

    frontend signup form

--- client/src/components/Log/SignUpForm.tsx
+++ client/src/components/Log/SignUpForm.tsx
@@ -1,10 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignUpForm = () => {
+  const [pseudo, setPseudo] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [controlPassword, setControlPassword] = useState("");
+
+  const handleRegister = async (e: React.MouseEvent<HTMLFormElement>) => {};
   return (
-    <div>
-      <h1>s'inscrire</h1>
-    </div>
+    <form action="" onSubmit={handleRegister} id="sign-up-form">
+      <label htmlFor="pseudo">Pseudo</label>
+      <br />
+      <input
+        type="text"
+        name="pseudo"
+        id="pseudo"
+        onChange={(e) => setPseudo(e.target.value)}
+        value={pseudo}
+      />
+      <div className="pseudo error"></div>
+      <br />
+      <label htmlFor="email">Email</label>
+      <br />
+      <input
+        type="text"
+        name="email"
+        id="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <div className="email error"></div>
+      <br />
+      <label htmlFor="password">Password</label>
+      <br />
+      <input
+        type="password"
+        name="password"
+        id="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+      <div className="password error"></div>
+      <br />
+      <label htmlFor="password-conf">Control Password</label>
+      <br />
+      <input
+        type="password"
+        name="password-conf"
+        id="password-conf"
+        onChange={(e) => setControlPassword(e.target.value)}
+        value={controlPassword}
+      />
+      <div className="password-confirm error"></div>
+      <br />
+      <input type="checkbox" id="terms" />
+      <label htmlFor="terms">
+        I accept terms{" "}
+        <a href="/" target="_blank" rel="noopener noreferrer">
+          GCD
+        </a>
+      </label>
+      <div className="terms error"></div>
+      <br />
+      <input type="submit" value="Valider inscription" />
+    </form>
   );
 };
 
